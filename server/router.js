c4a12f024813b0cb0354e9c40b0a934756705b14
Leonardo <leofdecarvalho@gmail.com>
2016-11-24 00:10:18 -0200

    authenticated server using "bcrypt-nodejs", "jwt-simple",  "passport" and  "passport-local".
    The data base used is mongodb with mongoose.
    
    The server express runs with nodemon, morgan and body-parser.

---
+++ server/router.js
@@ -0,0 +1,14 @@
+const Authentication = require('./controller/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {session:false});
+const requireSigin = passport.authenticate('local', {session:false});
+
+module.exports = function(app) {
+  app.get('/', requireAuth, function(req, res){
+    res.send({hi:'there'});
+  });
+  app.post('/signin', requireSigin, Authentication.signin);
+  app.post('/signup', Authentication.signup);
+}
